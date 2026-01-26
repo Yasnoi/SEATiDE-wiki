32de8f6ec7cb6c724958029effb5b9543fde77a8
Subilan <suyuanliang1995@gmail.com>
2026-01-26 16:30:20 +0800

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
 					sidebarPath: './sidebars.ts',
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
-					editUrl: 'https://github.com/seatidemc/wiki/tree/main/docs'
+					editUrl: 'https://github.com/seatidemc/wiki/tree/main'
 				},
 				blog: {
 					showReadingTime: true,
